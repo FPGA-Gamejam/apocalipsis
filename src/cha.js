6d8d5f805452ce8d054dd70a70152b1edffa1612
ignperez <ignperez@udec.cl>
2018-11-24 19:19:14 -0300

    sensor

--- src/cha.js
+++ src/cha.js
@@ -1,5 +1,7 @@
 var posY=false;
 var contact=true;
+var double=false; //false sin salto, true doble salto utilizado
+var sensors=[];
 
 function keyPressed(){
     if(keyCode == UP_ARROW){
@@ -12,25 +14,47 @@ class cha{
         this.x=x;
         this.y=y;
         this.r=r;
+        
+        //char
         this.personbody = new p2.Body({mass: 5, position: [this.x, this.y], fixedRotation: true});
-        this.personshape = new p2.Capsule({length:this.r ,radius: 2*this.r});
-        //this.personshape.sensor=true;
+        this.personshape = new p2.Circle({radius: this.r});
         this.personbody.addShape(this.personshape);
         world.addBody(this.personbody);
+        
+        //sensor
+        this.sensorbody = new p2.Body({mass: 5, position: [this.x, this.y]});
+        this.sensorshape = new p2.Circle({radius: 10});
+        this.sensorshape.sensor=true;
+        this.sensorbody.addShape(this.sensorshape);
+        world.addBody(this.sensorbody);
+        sensors.push(this.sensorbody);
         
         this.world=world;
     }
     update(){
+        //this.sensorbody.position[0]=this.personbody.position[0];
+        //this.sensorbody.position[1]=this.personbody.position[1]+this.r+2;
         this.world.on("endContact",function(event){
-            contact=false;
-            posY=false;
+            if(event.bodyA==sensors[0]){
+                console.log("a");
+                contact=false;
+                if(double==true){
+                    posY=false;
+                }
+            }
         });
         this.world.on("beginContact",function(event){
-            contact=true;
+            if(event.bodyA==sensors[0] || event.bodyB==sensors[0]){
+                contact=true;
+                double=false;
+            }
         });
-        if(posY==true && contact==true){
+        if(posY==true && double==false){
             this.personbody.applyImpulse([0,-1000]);
             posY=false;
+            if(contact==false){
+                double=true;
+            }
         }
         else{
             posY=false;
@@ -44,5 +68,6 @@ class cha{
     }
     draw(){
         drawBody(this.personbody);
+        drawBody(this.sensorbody);
     }
 } 
