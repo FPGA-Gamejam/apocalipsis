f2154f8bbe62dc49c5cfad2f90393e5ac1163cb0
ignperez <ignperez@udec.cl>
2018-11-24 22:38:03 -0300

    cosas

--- src/cha.js
+++ src/cha.js
@@ -3,8 +3,10 @@ var contact=true;
 var double=false; //false sin salto, true doble salto utilizado
 var sensors=[];
 var hit=false;
-var pum=false;
+var pum_d=false;
+var pum_i=false;
 var posX=true; //true derecha, false izquierda
+var count=0;
 
 function keyPressed(){
     if(keyCode == UP_ARROW){
@@ -16,7 +18,7 @@ function keyPressed(){
 }
 
 class cha{
-    constructor (x, y, r, world){
+    constructor (x, y, r, world, enemies){
         this.x=x;
         this.y=y;
         this.r=r;
@@ -64,26 +66,36 @@ class cha{
         sensors.push(this.hitbody_i);
         
         this.world=world;
+        this.enemies=enemies;
 
         this.world.on("endContact",function(event){
             if(event.bodyA==sensors[0] || event.bodyB==sensors[0] ){
-                 console.log(event);
-                contact=false;
-                if(double==true){
-                    posY=false;
+                count -= 1;
+                if (count <= 1) {
+                    contact=false;
+                    if(double==true){
+                        posY=false;
+                    }
                 }
             }
-            if(event.bodyA==sensors[1] || event.bodyB==sensors[1] || event.bodyA==sensors[2] || event.bodyB==sensors[2]){
-                pum=false;
+            if(event.bodyA==sensors[1] || event.bodyB==sensors[1]){
+                pum_d=false;
+            }
+            if(event.bodyA==sensors[2] || event.bodyB==sensors[2]){
+                pum_i=false;
             }
         });
         this.world.on("beginContact",function(event){
             if(event.bodyA==sensors[0] || event.bodyB==sensors[0]){
+                count += 1;
                 contact=true;
                 double=false;
             }
-            if(event.bodyA==sensors[1] || event.bodyB==sensors[1] || event.bodyA==sensors[2] || event.bodyB==sensors[2]){
-                pum=true;
+            if(event.bodyA==sensors[1] || event.bodyB==sensors[1]){
+                pum_d=true;
+            }
+            if(event.bodyA==sensors[2] || event.bodyB==sensors[2]){
+                pum_i=true;
             }
         });
     }
@@ -91,11 +103,12 @@ class cha{
         var vel = this.personbody.velocity;
         var pos = this.personbody.position;
         if(keyIsDown(RIGHT_ARROW)){
-            this.personbody.velocity = p2.vec2.fromValues(150, vel[1]);
+            this.personbody.velocity = p2.vec2.fromValues(200, vel[1]);
             posX=true;
         }
         else if(keyIsDown(LEFT_ARROW)){
-            this.personbody.velocity = p2.vec2.fromValues(-150, vel[1]);
+            console.log("izquierda");
+            this.personbody.velocity = p2.vec2.fromValues(-200, vel[1]);
             posX=false;
         }
         else{
@@ -117,19 +130,16 @@ class cha{
             posY=false;
         }
         //golpe
-        if(hit==true && pum==true){
-            if(posX=true){
-                console.log("golpe derecha");
-                hit=false;
-            }
-            else{
-                console.log("golpe izquierda");
-                hit=false;
-            }
+        if(hit==true && pum_d==true && posX==true){
+            console.log("golpe derecha");
+            hit=false;
+        }
+        if(hit==true && pum_i==true && posX==false){
+            console.log("golpe izquierda");
+            hit=false;
         }
     }
     draw(){
-        //drawBody(this.personbody);
         drawBody(this.hitbody);
         drawBody(this.hitbody_i);
         image(panda_idle, this.personbody.position[0] - 55, this.personbody.position[1] - 140);
--- src/level.js
+++ src/level.js
@@ -14,7 +14,7 @@
 			}
 		}, this);
 
-		this.cha = new cha(300, 500, 50, this.world);
+		this.cha = new cha(300, 500, 50, this.world, this.enemyarray);
 	}
 	update(dt) {
 		this.world.step(dt);
