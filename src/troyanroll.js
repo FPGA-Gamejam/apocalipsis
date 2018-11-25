0fcbd00ecf0a2f4b6210c71613253d6a6a296b68
wladivalenzuela <wlavalensaleconce@gmail.com>
2018-11-25 06:48:18 -0300

    troyanrolls update

--- src/troyanroll.js
+++ src/troyanroll.js
@@ -5,25 +5,30 @@ class Troyanroll extends Enemy{
 		this.pastNear = false;
 		this.nearNow  = false;
 		this.deltaChar = 150;
-		this.jumpSpeed = 1000;
+		this.jumpSpeed = 500;
+		this.jumpLocked = false;
+		this.horizontalRatio = 5;
 	}
 	update(dt) {
+			this.nearNow = this.charNear();
         if (this.stun) {
             this.stuntime -= dt;
             if (this.stuntime <= 0) {
                 this.stun = false;
             }
         };
-        
-		if(this.charLocked()&&this.body.velocity[1]<Math.abs(1))
+
+		if(this.charNear()&&this.body.velocity[1]<Math.abs(1))
 		{
 			this.body.velocity[1] = -this.jumpSpeed ;
+			this.jumpLocked	= true;
 			console.log("aca salto por cercano");
 		}
-	  // if(this.charNear() && this.level.cha.personbody.velocity[1]<-20 && this.body.velocity[1]<Math.abs(1) ){
-		// 	this.body.velocity[1] = -this.jumpSpeed ;
-		// 	console.log("aca salto por salto");
-		// }
+		if( this.level.cha.personbody.position[0] < this.body.position[0] )
+			this.body.velocity[0] = -Math.abs(this.body.velocity[1]/this.horizontalRatio);
+		else
+			this.body.velocity[0] = Math.abs(this.body.velocity[1]/this.horizontalRatio);
+
 
 		this.pastNear = this.nearNow;
 	}
@@ -41,7 +46,6 @@ class Troyanroll extends Enemy{
 	}
 
 	charLocked(){
-		this.nearNow = this.charNear();
 		if (!this.pastNear && this.nearNow){
 			return true;
 		}
