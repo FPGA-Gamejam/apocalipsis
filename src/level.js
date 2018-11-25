d8e14bb27c9e22926267acfa9d72b5136694ff82
qgerman2 <qgerman2@gmail.com>
2018-11-25 03:32:12 -0300

    troyan

--- src/level.js
+++ src/level.js
@@ -9,7 +9,7 @@
 		var enemies = svg.layer("npc");
 		enemies.forEach(function(obj) {
 			if (obj.type == "circle") {
-				var enemy = new Enemy(this, obj.x, obj.y);
+				var enemy = new Troyanpike(this, obj.x, obj.y);
 	        	this.enemyarray.push(enemy);
 			}
 		}, this);
--- src/troyanpike.js
+++ src/troyanpike.js
@@ -3,6 +3,52 @@ class Troyanpike extends Enemy{
 		super(level, x, y);
 		this.body.gravityScale = 0;
 		this.sensor.body.gravityScale = 0;
-		
+		this.pivotPositionY = x;
+		this.pivotPositionX = y;
+		//level.cha.position    [x,y]
+		//this.body.position
+		//this.body.applyImpulse([2000,2000]);
+		this.body.velocity = [10,-100];
+		this.deltaMovY = 20;
+		this.deltaMovX = 10;
+		this.deltaChar = 150;
+		this.chaseVelocity = 175;
+		this.backVelocity  = 75;
+
+
+	}
+
+	update(dt) {
+		if( this.charNear() ){
+			if( this.level.cha.personbody.position[0] < this.body.position[0] )
+				this.body.velocity[0]= -this.chaseVelocity ;
+			else
+				this.body.velocity[0]= this.chaseVelocity ;
+			if( this.level.cha.personbody.position[1] < this.body.position[1] )
+				this.body.velocity[1] = -this.chaseVelocity ;
+			else
+				this.body.velocity[1] = this.chaseVelocity ;
+		}
+		else{
+			if( this.body.position[1] > this.pivotPositionX + this.deltaMovY )
+				this.body.velocity[1] = -this.backVelocity;
+			if( this.body.position[1] < this.pivotPositionX - this.deltaMovY )
+				this.body.velocity[1] = this.backVelocity;
+			if( this.body.position[0] > this.pivotPositionY + this.deltaMovY )
+				this.body.velocity[0] = -this.backVelocity;
+			if( this.body.position[0] < this.pivotPositionY - this.deltaMovY )
+				this.body.velocity[0] = this.backVelocity;
+		}
+
+
+	}
+
+	charNear(){
+		if( Math.abs(this.level.cha.personbody.position[0] - this.body.position[0]) < this.deltaChar)
+		{
+		if( Math.abs(this.level.cha.personbody.position[1] - this.body.position[1]) < this.deltaChar)
+			return true;
+		}
+		return false;
 	}
 }
